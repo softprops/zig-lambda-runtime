ef1a07f54aea44bc1cc5f76f4252902dff738113
softprops <d.tangren@gmail.com>
2023-11-14 00:10:36 -0500

    declare module

--- build.zig
+++ build.zig
@@ -4,7 +4,7 @@ const std = @import("std");
 // Although this function looks imperative, note that its job is to
 // declaratively construct a build graph that will be executed by an external
 // runner.
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
@@ -16,19 +16,28 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "zig-lambda-runtime",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = .{ .path = "src/lambda.zig" },
-        .target = target,
-        .optimize = optimize,
+    // create a module to be used internally.
+    var lambda_module = b.createModule(.{
+        .source_file = .{ .path = "src/lambda.zig" },
     });
+
+    // register the module so it can be referenced
+    // using the package manager.
+    try b.modules.put(b.dupe("lambda"), lambda_module);
+
+    // const lib = b.addStaticLibrary(.{
+    //     .name = "lambda",
+    //     // In this case the main source file is merely a path, however, in more
+    //     // complicated build scripts, this could be a generated file.
+    //     .root_source_file = .{ .path = "src/lambda.zig" },
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
 
     // This declares intent for the library to be installed into the standard
     // location when the user invokes the "install" step (the default step when
     // running `zig build`).
-    b.installArtifact(lib);
+    //b.installArtifact(lib);
 
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
